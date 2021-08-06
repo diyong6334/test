bff21a044b843ebac41d2a37a7c33901af5153b9
diyong6334 <diyongstephen.com>
2021-08-06 11:05:53 +0100

    first commit

---
+++ list.py
@@ -0,0 +1,59 @@
+#11111111111111111111111111111111111111111111111111
+def sum():
+     a= input('enter a first number')
+     b= input('enter a  second number')
+     c= input('enter a third number')
+    if a=b
+    result= a+b+c
+    else
+    return result
+sum()
+
+#222222222222222222222222222222222222222222222222222
+def prime():
+    a= input('enter a number')
+    if a % 2 == 0:
+        print(a +'is a not a prime number')
+    else:
+        print(a +'is a a prime number')
+prime()
+ #44444444444444444444444444444444444444444444444444444444
+def func (amount):
+    num = input('enter a number')
+    for (i=0, i<amount.length; ++i):
+        total = +=amount[i]
+        return total
+fun()
+
+    
+        
+   
+   
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+
+
+
+
+
+
+
+
+
+
+
